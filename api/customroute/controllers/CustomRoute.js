2b31438592d9da4fca4be1356087ce0011251191
Juni Yadi <juniyadi94@gmail.com>
2019-11-03 15:49:18 +0700

    Create Custome Routes API For Override HomePages

---
+++ api/customroute/controllers/CustomRoute.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Read the documentation () to implement custom controller functions
+ */
+
+module.exports = {
+    redirect: async ctx => {
+        ctx.redirect('/gdidashboard');
+    }
+};
