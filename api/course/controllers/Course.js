26a8f8a4b10c0bbad2c4f1a90f599c84c7d5ae71
Juni Yadi <juniyadi94@gmail.com>
2019-11-12 11:40:01 +0700

    Added Routes Show data Reference by JWT UserID

--- api/course/controllers/Course.js
+++ api/course/controllers/Course.js
@@ -4,4 +4,19 @@
  * Read the documentation () to implement custom controller functions
  */
 
-module.exports = {};
+module.exports = {
+    me: async(ctx) => {
+        const user = ctx.state.user;    
+        if (!user) {
+          return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
+        }
+    
+        const data = await strapi.services.course.find({user:user.id});  
+    
+        if(!data){
+          return ctx.notFound();
+        }
+    
+        ctx.send(data);
+    }
+};
