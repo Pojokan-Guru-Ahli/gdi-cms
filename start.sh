29b22e4055c1ef03bfa170e2ce5e8d3c4500e819
EC2 Default User <ec2-user@ip-172-31-8-144.ap-southeast-1.compute.internal>
2019-11-16 19:54:35 +0000

    Add start script.

---
+++ start.sh
@@ -0,0 +1,1 @@
+NODE_ENV=production APP_HOST=cms.gdi.pojokan.io /home/ec2-user/.nvm/versions/node/v10.17.0/bin/node /home/ec2-user/.nvm/versions/node/v10.17.0/lib/node_modules/yarn/bin/yarn.js start
