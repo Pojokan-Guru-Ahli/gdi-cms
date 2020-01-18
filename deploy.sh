1b9d557032236620737534739c246e6cbee5bd97
Suherman <suherman@qantas.com.au>
2020-01-18 23:26:13 +1100

    Add webconfig deployment.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-curl -X POST http://cms.gdi.pojokan.io/sync/v1/sync -H 'Content-Type: application/json' -d '{"file": "artifact.zip", "service": "cms.service", "destination": "gdi-cms"}'
+curl -X POST http://cms.gdi.pojokan.io/sync/v1/sync -H 'Content-Type: application/json' -d '{"webConfig": "nginx.conf", "file": "artifact.zip", "service": "cms.service", "destination": "gdi-cms", "bucket": "cms.gdi.pojokan.io"}'
