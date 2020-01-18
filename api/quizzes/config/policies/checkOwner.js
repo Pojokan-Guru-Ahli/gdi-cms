241671940cef01a7f494e55dab7a7265b0efaf0b
Juni Yadi <juniyadi94@gmail.com>
2020-01-18 08:50:05 +0700

    Update Api Resicted by User Teacher For Quizzes

---
+++ api/quizzes/config/policies/checkOwner.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * `checkOwner` policy.
+ */
+
+module.exports = async (ctx, next) => {
+
+  const { role, id } = ctx.state.user;
+
+  if (role.name == "Teacher") {
+    const { body } = ctx.request
+
+    const fieldId = body.id
+    
+    Quizzes.findOne({
+      id: fieldId,
+      user: id
+    }).then(result => {
+      if(!result) {
+        return ctx.unauthorized(`You're not allowed to perform this action!`)
+      }
+    })
+
+    await next()
+  } else {
+    ctx.unauthorized(`You're not allowed to perform this action!`)
+  }
+};
