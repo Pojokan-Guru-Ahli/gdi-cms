33e7a6711a78afbc07c1af2504d75cd1f83f9c6e
Suherman <suherman@qantas.com.au>
2020-02-09 07:28:01 +1100

    Add staging environment.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
+set -ex
 curl -X POST https://cms.gdi.pojokan.io/sync/v1/sync -H 'Content-Type: application/json' -d '{"webConfig": "nginx.conf", "file": "artifact.zip", "service": "cms.service", "destination": "gdi-cms", "bucket": "cms.gdi.pojokan.io","region": "ap-southeast-1"}'
