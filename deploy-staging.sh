c67a7eaa3da249ca5ed2d4c10a974b6346d5afdd
Suherman <suherman@qantas.com.au>
2020-02-09 09:21:25 +1100

    Add staging build.

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,2 +1,2 @@
 set -ex
-curl -X POST https://cms.gdi.pojokan.io/sync/v1/sync -H 'Content-Type: application/json' -d '{"webConfig": "nginx.conf", "file": "artifact.zip", "service": "cms-staging.service", "destination": "gdi-cms-staging", "bucket": "cms.gdi.pojokan.io","region": "ap-southeast-1"}'
+curl -X POST https://cms.gdi.pojokan.io/sync/v1/sync -H 'Content-Type: application/json' -d '{"webConfig": "nginx.conf", "file": "artifact-staging.zip", "service": "cms-staging.service", "destination": "gdi-cms-staging", "bucket": "cms.gdi.pojokan.io","region": "ap-southeast-1"}'
