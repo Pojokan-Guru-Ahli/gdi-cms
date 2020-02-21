0b27393af581c71e31351bdcf54d137d19dc2745
JuniYadi <juniyadi94@gmail.com>
2020-02-22 02:04:32 +0700

    BE-38 fix(chapter) - Fixed checkOwner For REST API and Graphql

--- api/chapter/config/policies/checkOwnerGQL.js
+++ api/chapter/config/policies/checkOwnerGQL.js
@@ -4,16 +4,16 @@ const gdiQuery = require('../../../../config/policies/checkOwner')
 const _ = require('lodash')
 
 /**
- * `checkOwner` policy.
+ * `checkOwnerGQL` policy.
  */
-
 
 module.exports = async (ctx, next) => {
   // check user permission from onefiles global policy
   const checkUser = await gdiQuery('chapter', strapi, ctx)
-  
-  if(_.isEmpty(checkUser)) {
-    ctx.unauthorized(`You're not allowed to perform this action!`)
+
+  // continue if found, and unauthorized if null or empty
+  if (_.isEmpty(checkUser)) {
+    throw new Error(`You're not allowed to perform this action!`)
   } else {
     await next()
   }
